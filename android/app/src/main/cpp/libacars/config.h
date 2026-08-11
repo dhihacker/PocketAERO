57499eb2fe116738f0234952f4c498eac4722b51
dhihacker <59767833+dhihacker@users.noreply.github.com>
2026-08-12 04:48:57 +0500

    Add missing config.h for libacars library

---
+++ android/app/src/main/cpp/libacars/config.h
@@ -0,0 +1,51 @@
+/*
+ * config.h - Generated configuration header for libacars
+ * This file defines platform-specific settings for the libacars library
+ */
+
+#ifndef LIBACARS_CONFIG_H
+#define LIBACARS_CONFIG_H
+
+/* Define if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define if you have the <time.h> header file. */
+#define HAVE_TIME_H 1
+
+/* Define if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define if you have the <ctype.h> header file. */
+#define HAVE_CTYPE_H 1
+
+/* Define if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define if you have the strsep function. */
+#define HAVE_STRSEP 1
+
+/* Define if you have the strtoll function. */
+#define HAVE_STRTOLL 1
+
+/* Define if you have the strtoull function. */
+#define HAVE_STRTOULL 1
+
+/* LibXML2 support */
+#undef WITH_LIBXML2
+
+/* Zlib support */
+#undef WITH_ZLIB
+
+#endif /* LIBACARS_CONFIG_H */
